62b3ce06bc08ec39aff04f527553f2df823d71e2
Chekos10 <119345370+Chekos10@users.noreply.github.com>
2023-08-03 00:13:42 +0300

    finnal_save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,12 @@ import { Statistics } from './Statistic/Statistics';
 export const App = () => {
   return <Feedback />;
 };
+
 export const Feedback = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
   const onLeaveFeedback = options => {
     switch (options){
       case 'good':
@@ -24,10 +26,12 @@ export const Feedback = () => {
           break;
     }
   };
+
   const arrOption = ['good', 'neutral','bad']
   const countTotalFeedback = () => {
     return good + neutral + bad;
   };
+
   const countPositiveFeedbackPercentage = () => {
     return Math.round((good / countTotalFeedback()) * 100);
   };
